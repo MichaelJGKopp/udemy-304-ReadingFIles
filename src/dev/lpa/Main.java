fb110acc28c72ea1672335008c798be1cf89a879
Michael Kopp <MichaelJG.Kopp@gmail.com>
2024-10-15 20:23:11 +0200

    read into char array at once

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -9,10 +9,11 @@ public class Main {
 
     // reads one integer at a time, meaning 4 bytes in java
     try (FileReader reader = new FileReader("file.txt")) {  // default buffer size, being loaded at once
-
+      char[] block = new char[1_000];
       int data;
-      while ((data = reader.read()) != -1) {  // end reached when -1
-        System.out.println((char) data);  // first was disc read, else buffered read from memory
+      while ((data = reader.read(block)) != -1) {  // end reached when -1
+        String content = new String(block, 0, data);
+        System.out.printf("---> [%d chars] %s%n", data, content);  // first was disc read, else buffered read from memory
       }
     } catch (IOException e) {
       e.printStackTrace();
